96aac25acece9edde20a0aac66df0fd2f148306e
LriKrj <lauri.karjalainen3@hotmail.com>
2022-09-04 18:29:44 +0300

    valmis

--- App.js
+++ App.js
@@ -16,26 +16,24 @@ export default function App() {
   const [calchistory, setCalchistory] = useState([]);
 
   const addPressed = () => {
-    const sum = parseInt(number1) + parseInt(number2);
-    setResult(sum);
-   
-    setCalchistory([...calchistory, [number1, " + ", number2, " = ", sum]]);
+    const operation= (number1 + " + " + number2 + " = " + (parseInt(number1) + parseInt(number2)));
+    const adding= [...calchistory,{key:operation}];
+    setCalchistory(adding)
+    setNumber1("")
+    setNumber2("")
+
   };
   const minusPressed = () => {
-    const minus = parseInt(number1) - parseInt(number2);
-    setResult(minus);
-    [
-      ...calchistory,
-      [number1, " - ", number2, " = ", minus],
-    ];
-    setCalchistory([
-      ...calchistory,
-      [number1, " - ", number2, " = ", minus],
-    ]);
+    const operation= (number1 + " - " + number2 + " = " + (parseInt(number1) - parseInt(number2)));
+    const minus= [...calchistory,{key:operation}];
+    setCalchistory(minus)
+    setNumber1("")
+    setNumber2("")
   };
 
   return (
     <View style={styles.container}>
+      <View style={styles.container}>
       <Text>Result: {result}</Text>
       <TextInput
         keyboardType="numeric"
@@ -49,7 +47,7 @@ export default function App() {
         onChangeText={(number) => setNumber2(number)}
         value={number2}
       />
-
+      
       <View style={styles.operator}>
         <View style={styles.button}>
           <Button onPress={addPressed} title="+" />
@@ -58,39 +56,41 @@ export default function App() {
           <Button onPress={minusPressed} title="-" />
         </View>
       </View>
-
-      <Text>History:</Text>
-      <FlatList
+          
+          <Text> History</Text>
+      <FlatList style={styles.list} 
         data={calchistory}
-        renderItem={({ item }) => {
-          return <Text>{item}</Text>;
-        }}
-      />
-
-      <StatusBar style="auto" />
+        renderItem={({ item }) => 
+          <Text>{item.key}</Text>}
+          keyExtractor={(item, index) => index.toString()}
+       />    
+        <StatusBar style="auto" />
     </View>
+    </View>
+      
   );
 }
 
+
+
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    marginTop: 100
+    backgroundColor: "#fff",
+    alignItems: "center",
+    marginTop: 100,
   },
-  operator: {
-    flexDirection: 'row',
-    
-  },
-  input: {
-    margin: 5,
-    width: 150, 
-    borderColor: 'black', 
-    borderWidth: 1,
-  },
-  button: {
-    width: 50,
-    borderColor: 'black',
-    margin: 10
-  }
-});
+    operator: {
+    flexDirection: "row",
+    },
+    input: {
+      margin: 5,
+      width: 150,
+      borderColor: "black",
+      borderWidth: 1,
+    },
+    button: {
+      width: 50,
+      borderColor: "black",
+      margin: 10,
+    },
+  });
